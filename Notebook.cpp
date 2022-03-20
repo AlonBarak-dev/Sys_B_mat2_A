026838e5672c71981b4d8b27d83c78c619e61641
Alon Barak <alonbarak3110@gmail.com>
2022-03-20 20:22:04 +0200

    ready to submit part A

--- Notebook.cpp
+++ Notebook.cpp
@@ -8,16 +8,16 @@ using namespace std;
 namespace ariel{
 
     void Notebook::write(int page, int row, int column, ariel::Direction dircetion , string text){
-        return;
+        
     }
     string Notebook::read(int page, int row, int column, ariel::Direction dircetion , int length){
         return "";
     }
     void Notebook::erase(int page, int row, int column, ariel::Direction dircetion , int length){
-        return;
+        
     }
     void Notebook::show(int page){
-        return;
+        
     }
 
 
