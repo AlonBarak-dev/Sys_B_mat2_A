b2a95eb014eabcc0456c40c5fe1ac380b95dbb67
Alon Barak <alonbarak3110@gmail.com>
2022-03-22 01:30:38 +0200

    final part A

--- Notebook.cpp
+++ Notebook.cpp
@@ -3,9 +3,54 @@
 #include <string>
 
 using namespace std;
+using namespace ariel;
 
 
 namespace ariel{
+
+    // Page::Page(){
+    //     this->number_of_lines = 0;
+    // }
+
+    // string Page::get_line(int line){
+    //     // return the line if exist
+    //     if (this->lines.find(line) == this->lines.end())
+    //     {
+    //         return this->lines.at(line);
+    //     }
+    //     // return a line of 100 "_"
+    //     else{
+    //         string empty;
+    //         for (int i = 0; i < 100; i++)
+    //         {
+    //             empty.append("_");
+    //         }
+    //         return empty;
+    //     }
+    // }
+
+    // string Page::get_column(int column, int length, int start){
+
+    //     string col_str;
+    //     for (int i = start; i < length; i++)
+    //     {
+    //         if (this->lines.find(i) == this->lines.end())
+    //         {
+    //             col_str.push_back(this->lines.at(i).at(column));
+    //         }
+    //         else{
+    //             col_str.push_back('_');
+    //         }
+    //     }
+    //     return col_str;
+
+
+    // }
+
+
+    Notebook::Notebook(){
+        this->number_of_pages = 0;
+    }
 
     void Notebook::write(int page, int row, int column, ariel::Direction dircetion , string text){
         
@@ -19,6 +64,5 @@ namespace ariel{
     void Notebook::show(int page){
         
     }
-
 
 }
--- Notebook.hpp
+++ Notebook.hpp
@@ -3,12 +3,32 @@
 
 #include "Direction.hpp"
 #include <string>
+#include <unordered_map>
 
 namespace ariel
 {
+    class Page{
+
+        private:
+            std::unordered_map<int, std::string> lines;
+            int number_of_lines;
+        
+        public:
+            Page();
+            std::string get_line(int line);
+            std::string get_column(int column, int length, int start);
+            void set_line(int line, std::string data);
+    };
+
+
+
     class Notebook
     {
+        private:
+            std::unordered_map<int, Page> pages;
+            int number_of_pages;
         public:
+            Notebook();
             void write(int page, int row, int column, ariel::Direction dircetion , std::string text);
             std::string read(int page, int row, int column, ariel::Direction dircetion , int length);
             void erase(int page, int row, int column, ariel::Direction dircetion , int length);
