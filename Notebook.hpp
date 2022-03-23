64097bd44f49d006a7cc0fce2dcd10224911fdbc
Alon Barak <alonbarak3110@gmail.com>
2022-03-23 19:49:51 +0200

    uhhhh

--- Notebook.hpp
+++ Notebook.hpp
@@ -7,37 +7,37 @@
 
 namespace ariel
 {
-    class Page{
+    // class Page{
 
-        private:
-            std::unordered_map<unsigned int, std::string> lines;
-            int number_of_lines;
-            unsigned int max_row;
+    //     private:
+    //         std::unordered_map<unsigned int, std::string> lines;
+    //         int number_of_lines;
+    //         unsigned int max_row;
         
-        public:
-            Page();
-            std::string get_line(unsigned int line);
-            std::string get_column(unsigned int column, int length, unsigned int start);
-            unsigned int get_max_line();
-            void set_line(unsigned int line, unsigned int column, std::string data);
-            void set_column(unsigned int line, unsigned int column, int length, std::string data);
-            void create_line(unsigned int line_num);
+    //     public:
+    //         Page();
+    //         std::string get_line(unsigned int line);
+    //         std::string get_column(unsigned int column, int length, unsigned int start);
+    //         unsigned int get_max_line();
+    //         void set_line(unsigned int line, unsigned int column, std::string data);
+    //         void set_column(unsigned int line, unsigned int column, int length, std::string data);
+    //         void create_line(unsigned int line_num);
 
-    };
+    // };
 
 
 
     class Notebook
     {
         private:
-            std::unordered_map<unsigned int, Page> pages;
+            // std::unordered_map< int, Page> pages;
             int number_of_pages;
         public:
             Notebook();
-            void write(unsigned int page,unsigned int row,unsigned int column, ariel::Direction dircetion , std::string text);
-            std::string read(unsigned int page,unsigned int row,unsigned int column, ariel::Direction dircetion , int length);
-            void erase(unsigned int page,unsigned int row,unsigned int column, ariel::Direction dircetion , int length);
-            void show(unsigned int page);
+            void write( int page, int row, int column, ariel::Direction dircetion , std::string text);
+            std::string read( int page, int row, int column, ariel::Direction dircetion , int length);
+            void erase( int page, int row, int column, ariel::Direction dircetion , int length);
+            void show( int page);
     };
 }
 
